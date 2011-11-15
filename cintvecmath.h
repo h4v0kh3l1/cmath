cf6d70c9777e42c86df3303be5d1222ddd00f06b
Heli Wang <heli.h101@gmail.com>
2011-11-15 16:54:17 -0500

    Restructured cintvec.

---
+++ cintvecmath.h
@@ -0,0 +1,31 @@
+//
+//  vecmath.h
+//  cmath
+//
+//  Created by Heli Wang on 11/15/11.
+//  Copyright (c) 2011 Carnegie Mellon. All rights reserved.
+//
+
+
+#ifndef cmath_vecmath_h
+#define cmath_vecmath_h
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include "cintmath.h"
+
+typedef struct cintvec* cintvec;
+struct cintvec{
+	int length;
+	cint* carray;
+};
+
+bool is_cintvec(cintvec a);
+bool can_add_cintvec(cintvec a, cintvec b);
+cintvec new_cintvec(cint* carray, int l);
+void free_cintvec(cintvec a);
+cintvec add_cintvec(cintvec a, cintvec b);
+
+
+
+#endif
